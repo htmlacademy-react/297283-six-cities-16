55bd885ed189ececbbbe75c653df2c20bff3088e
Denis Obolevich <denzelwash33@yandex.ru>
2024-08-09 22:09:33 +0300

    Подключил thunk

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { offersSlice } from './slices/offers'
+import { createAPI } from '../services/api'
+
+const api = createAPI()
 
 const store = configureStore({
 	reducer: {
 		[offersSlice.name]: offersSlice.reducer
-	}
+	},
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({
+			thunk: {
+				extraArgument: api
+			}
+		})
 })
 
 export type RootState = ReturnType<typeof store.getState>
