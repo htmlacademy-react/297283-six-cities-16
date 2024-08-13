2dc8f65994759c541ab684f453d6e3e5a52f71f3
Denis Obolevich <denzelwash33@yandex.ru>
2024-08-13 19:05:16 +0300

    Показ заглушки на главной если нет офферов

--- src/components/main/empty-offers/empty-offers.tsx
+++ src/components/main/empty-offers/empty-offers.tsx
@@ -1,9 +1,15 @@
-export default function EmptyOffers(): JSX.Element {
+import { CityName } from '../../../types/location'
+
+type EmptyOffersProps = {
+	city: CityName
+}
+
+export default function EmptyOffers({ city }: EmptyOffersProps): JSX.Element {
 	return (
 		<section className="cities__no-places">
 			<div className="cities__status-wrapper tabs__content">
 				<b className="cities__status">No places to stay available</b>
-				<p className="cities__status-description">We could not find any property available at the moment in Dusseldorf</p>
+				<p className="cities__status-description">We could not find any property available at the moment in {city}</p>
 			</div>
 		</section>
 	)
