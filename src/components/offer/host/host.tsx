f710c00d23ef2d080cf105ac36fb2aa6de714f8e
Denis Obolevich <denzelwash33@yandex.ru>
2024-08-16 22:12:08 +0300

    На странице представлена расширенная информация об объекте для аренды (обычное предложение)

--- src/components/offer/host/host.tsx
+++ src/components/offer/host/host.tsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames'
 import { UserShort } from '../../../types/user'
 
 type HostProps = {
@@ -12,7 +13,7 @@ export default function Host({ host, description }: HostProps): JSX.Element {
 		<div className="offer__host">
 			<h2 className="offer__host-title">Meet the host</h2>
 			<div className="offer__host-user user">
-				<div className="offer__avatar-wrapper offer__avatar-wrapper--pro user__avatar-wrapper">
+				<div className={classNames('offer__avatar-wrapper user__avatar-wrapper ', { 'offer__avatar-wrapper--pro': host.isPro })}>
 					<img className="offer__avatar user__avatar" src={avatarUrl} width={74} height={74} alt={`${name} avatar`} />
 				</div>
 				<span className="offer__user-name">{name}</span>
