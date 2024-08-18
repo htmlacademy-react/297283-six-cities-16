a1bdda603fb65efe08ad6dc64044224fced51411
Denis Obolevich <denzelwash33@yandex.ru>
2024-08-18 15:06:49 +0300

    Добавил показ ошибок

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import App from './components/app/app'
 import { Provider } from 'react-redux'
 import store from './store/store'
 import { checkAuth } from './store/thunks/auth'
+import { Toaster } from 'sonner'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
@@ -13,6 +14,7 @@ root.render(
 	<React.StrictMode>
 		<Provider store={store}>
 			<App />
+			<Toaster position="top-right" richColors />
 		</Provider>
 	</React.StrictMode>
 )
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,27 @@
-import axios, { AxiosInstance } from 'axios'
+import axios, { AxiosError, AxiosInstance, isAxiosError } from 'axios'
 import { getToken } from './token'
+import { toast } from 'sonner'
+
+enum ErrorType {
+	Common = 'COMMON_ERROR',
+	Validation = 'VALIDATION_ERROR'
+}
+
+interface ErrorBody {
+	errorType: ErrorType
+	message: string
+	details: [
+		{
+			property: string
+			value: string
+			messages: [string]
+		}
+	]
+}
+
+function isAxiosErrorCustom<T>(error: unknown): error is AxiosError<T> {
+	return isAxiosError(error)
+}
 
 const API_URL = 'https://16.design.htmlacademy.pro/six-cities'
 const REQUEST_TIMEOUT = 5000
@@ -17,6 +39,22 @@ export const createAPI = (): AxiosInstance => {
 			config.headers[TOKEN_HEADER_NAME] = token
 		}
 		return config
+	})
+
+	api.interceptors.response.use(null, (error) => {
+		if (isAxiosErrorCustom<ErrorBody>(error)) {
+			const errorResponse = error.response
+			if (errorResponse) {
+				if (errorResponse.status >= 500) {
+					toast.error('Server error')
+					return
+				}
+				if (errorResponse.data.errorType === ErrorType.Validation) {
+					const validationMessage = errorResponse.data.details[0].messages[0]
+					toast.error(validationMessage)
+				}
+			}
+		}
 	})
 
 	return api
